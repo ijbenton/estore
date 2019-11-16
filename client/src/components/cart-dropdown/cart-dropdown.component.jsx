07041ee1b7475c8adc1a3ad107923a8f7d0e7a78
ijbenton <bentonian1@gmail.com>
2019-11-15 22:30:37 -0800

    deleted unnecessary variables

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,6 @@ import { withRouter } from 'react-router-dom';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/header/header.actions';
-import { selectCartHidden } from '../../redux/header/header.selectors';
 
 import {
   CartDropdownContainer,
@@ -15,26 +14,22 @@ import {
   CartItemsContainer
 } from './cart-dropdown.styles';
 
-const CartDropdown = ({ cartItems, history, dispatch, hidden }) => {
+const CartDropdown = ({ cartItems, history, toggleCartHidden }) => {
   const node = useRef();
   const handleClick = e => {
     if (node.current.contains(e.target)) {
       // inside click
-      console.log('dropdown handleClick fired');
       return;
     }
     // outside click
-    console.log('outside click of cart dropdown');
-    dispatch(toggleCartHidden());
+    toggleCartHidden();
   };
 
   useEffect(() => {
     // add when mounted
-    console.log('cart dropdown click event added');
     document.addEventListener('mousedown', handleClick);
     // return function to be called when unmounted
     return () => {
-      console.log('cart dropdown click event removed');
       document.removeEventListener('mousedown', handleClick);
     };
   }, []);
@@ -52,7 +47,7 @@ const CartDropdown = ({ cartItems, history, dispatch, hidden }) => {
       <CartDropdownButton
         onClick={() => {
           history.push('/checkout');
-          dispatch(toggleCartHidden());
+          toggleCartHidden();
         }}
       >
         GO TO CHECKOUT
@@ -60,9 +55,13 @@ const CartDropdown = ({ cartItems, history, dispatch, hidden }) => {
     </CartDropdownContainer>
   );
 };
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  hidden: selectCartHidden
+  cartItems: selectCartItems
 });
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartDropdown)
+);
--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,6 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/header/header.actions';
-import { selectCartHidden } from '../../redux/header/header.selectors';
 
 import {
   CartIconContainer,
@@ -12,24 +11,20 @@ import {
   ItemCountContainer
 } from './cart-icon.styles';
 
-const CartIcon = ({ toggleCartHidden, itemCount, dispatch, hidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   const node = useRef();
   const handleClick = e => {
     if (node.current.contains(e.target)) {
       // inside click
-      console.log('carticon handleClick fired');
       toggleCartHidden();
       return;
     }
     // outside click
-    console.log('outside click of cart icon');
   };
   useEffect(() => {
-    console.log('cart icon click event added');
     document.addEventListener('mousedown', handleClick);
     // return function to be called when unmounted
     return () => {
-      console.log('cart icon click event removed');
       document.removeEventListener('mousedown', handleClick);
     };
   }, []);
@@ -45,7 +40,6 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount,
-  hidden: selectCartHidden
+  itemCount: selectCartItemsCount
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
